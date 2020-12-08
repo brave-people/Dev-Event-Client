1007197a241959baa60f7db783dc544d7400458b
KoEonYack <21300035@handong.edu>
2020-12-08 21:52:59 +0900

    Updates

---
+++ precache-manifest.e52d20a59b4a735b140a8ab231477a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654e92c0634dfeafe92499003e6b09ce",
+    "url": "/Dev-Event-Client/index.html"
+  },
+  {
+    "revision": "0cdb21a1e16a5f9fdf67",
+    "url": "/Dev-Event-Client/static/css/main.caa66d76.chunk.css"
+  },
+  {
+    "revision": "43f7f1177017aa051f4c",
+    "url": "/Dev-Event-Client/static/js/2.8766c6b7.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Dev-Event-Client/static/js/2.8766c6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdb21a1e16a5f9fdf67",
+    "url": "/Dev-Event-Client/static/js/main.cfecc87f.chunk.js"
+  },
+  {
+    "revision": "11407c2c31ca0855bc9a",
+    "url": "/Dev-Event-Client/static/js/runtime-main.fc2c1060.js"
+  }
+]);
