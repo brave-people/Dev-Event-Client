f439a100060d5412eca1dcb745aafae1b743ef59
woohwahwa <1233wsh@gmail.com>
2021-01-01 15:21:45 +0900

    modify calendar list

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -16,7 +16,7 @@ function Navigation(props: NavigationProps) {
   const { date } = useSelector<ReducerType>(state => state.month) as MonthModel;
   const dispatch = useDispatch();
   let [show, showMonth] = useState(false);
-  const startDate = new Date('2020.09.01');
+  const startDate = new Date('2020-09-01');
   const monthArr: string[] = []
   const disableDate = ['2020.09', '2020.10', '2020.11', '2021.02'];
   for (let i = startDate.getMonth() + 1; i <= 12; i++) {
