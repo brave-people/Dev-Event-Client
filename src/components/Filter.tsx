014ff2dbcf4cdb96a09d2410056d3bd60d6a06e7
leedoopal <1233wsh@gmail.com>
2021-02-26 11:21:46 +0900

    쓰지 않는 코드 제거

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,52 +3,74 @@ import { useDispatch, useSelector } from "react-redux";
 import { ReducerType } from "../rootReducer";
 import { activeFilter } from "../slices/filter";
 import { FilterModel } from "../models/filter";
-import { MonthModel } from "../models/month";
 
 function Filter() {
-  const { tags } = useSelector<ReducerType>(state => state.filter) as FilterModel;
-  const { activeTags } = useSelector<ReducerType>(state => state.filter) as FilterModel;
-  const { showMonth } = useSelector<ReducerType>(state => state.month) as MonthModel;
+  const { tags } = useSelector<ReducerType>(
+    (state) => state.filter
+  ) as FilterModel;
+  const { activeTags } = useSelector<ReducerType>(
+    (state) => state.filter
+  ) as FilterModel;
   const dispatch = useDispatch();
   let [showTags, setTags] = useState(false);
 
-  // month 클릭 한 경우 필터 닫기
-  if (showMonth && showTags) {
-    setTags(false)
-  }
-
   function setShowTags() {
-    setTags(!showTags)
+    setTags(!showTags);
   }
 
   function setTag(tag: string) {
-    dispatch(activeFilter(tag))
+    dispatch(activeFilter(tag));
   }
 
   return (
-    <section className={ 'filter__box' }>
-      <article className={ 'filter__button__wrap' }>
-        <button className={ 'filter__button' } onClick={ () => setTag('온라인') }>온라인</button>
-        <button className={ 'filter__button' } onClick={ () => setTag('컨퍼런스') }>컨퍼런스</button>
-        <button className={ 'filter__button' } onClick={ () => setTag('세미나') }>세미나</button>
-        <button className={ 'filter__button--icon' } onClick={ () => setShowTags() }>
+    <section className={"filter__box"}>
+      <article className={"filter__button__wrap"}>
+        <button className={"filter__button"} onClick={() => setTag("온라인")}>
+          온라인
+        </button>
+        <button className={"filter__button"} onClick={() => setTag("컨퍼런스")}>
+          컨퍼런스
+        </button>
+        <button className={"filter__button"} onClick={() => setTag("세미나")}>
+          세미나
+        </button>
+        <button
+          className={"filter__button--icon"}
+          onClick={() => setShowTags()}
+        >
           <img
-            src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/filter.svg" alt="filter"/>
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/filter.svg"
+            alt="filter"
+          />
         </button>
       </article>
-      <div className={ showTags ? 'filter--background' : '' } onClick={ () => setShowTags() }></div>
-      <article className={ showTags ? 'filter filter--active' : 'filter' }>
-        <p className={ 'filter__title' }>태그</p>
-        <div className={ 'filter--grid' }>
-          { tags.map((el: string, index: number) => {
-            return <p onClick={ () => setTag(el) } key={ index }
-                      className={ activeTags.find(tag => tag === el) ? 'tag tag--active' : 'tag' }>{ el }</p>
-          }) }
+      <div
+        className={showTags ? "filter--background" : ""}
+        onClick={() => setShowTags()}
+      ></div>
+      <article className={showTags ? "filter filter--active" : "filter"}>
+        <p className={"filter__title"}>태그</p>
+        <div className={"filter--grid"}>
+          {tags.map((el: string, index: number) => {
+            return (
+              <p
+                onClick={() => setTag(el)}
+                key={index}
+                className={
+                  activeTags.find((tag) => tag === el)
+                    ? "tag tag--active"
+                    : "tag"
+                }
+              >
+                {el}
+              </p>
+            );
+          })}
         </div>
-        <div className={ 'filter--pointer' }></div>
+        <div className={"filter--pointer"}></div>
       </article>
     </section>
-  )
+  );
 }
 
 export default Filter;
