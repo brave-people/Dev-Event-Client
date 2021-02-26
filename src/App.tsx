e1017eb2a26becbc7336298fd73421fa06e64096
leedoopal <1233wsh@gmail.com>
2021-02-26 11:22:06 +0900

    검색기능추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import SplashScreen from './components/Splash'
-import Home from './pages/Home'
-import Education from './pages/Education'
-import Navigation from "./components/Navigation";
-import './App.scss';
-import './styles/index.scss'
+import SplashScreen from "./components/Splash";
+import Home from "./pages/Home";
+import ProductPast from "./pages/ProductPast";
+
+import "./App.scss";
+import "./styles/index.scss";
+
 import { useDispatch, useSelector } from "react-redux";
 import { ReducerType } from "./rootReducer";
 import { MonthModel } from "./models/month";
@@ -14,59 +15,63 @@ import { activeMonth } from "./slices/month";
 function App() {
   const dispatch = useDispatch();
   const [show, setShow] = useState(false);
-  const { date } = useSelector<ReducerType>(state => state.month) as MonthModel;
+  const { date } = useSelector<ReducerType>(
+    (state) => state.month
+  ) as MonthModel;
 
-  document.addEventListener('mousemove', (e) => {
-    let circle = document.getElementById('circle');
-    if (circle) circle.style.transform = `translate(${ e.x }px, ${ e.y }px)`
+  document.addEventListener("mousemove", (e) => {
+    let circle = document.getElementById("circle");
+    if (circle) circle.style.transform = `translate(${e.x}px, ${e.y}px)`;
   });
 
   // 최초 날짜 설정
-  const addZero = Number(new Date().getMonth() + 1) < 10 ? '0' : '';
+  const addZero = Number(new Date().getMonth() + 1) < 10 ? "0" : "";
   if (!date) {
-    const currentDate = `${ new Date().getFullYear() }.${ addZero }${ Number(new Date().getMonth() + 1) }`
-    dispatch(activeMonth(currentDate))
+    const currentDate = `${new Date().getFullYear()}.${addZero}${Number(
+      new Date().getMonth() + 1
+    )}`;
+    dispatch(activeMonth(currentDate));
   }
 
   return (
     <div>
-      <div id={ 'circle' }></div>
-      <SplashScreen/>
-      <div className={ "header" }>
-        <img id={ 'logo' } src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/dev-128.png"
-             alt="logo"/>
-        <div className={ 'hambuger' } onClick={ () => setShow(!show) }>
-          <div className={ 'hambuger__icon' }>
-            <span className={ 'hambuger__icon--line' }></span>
-            <span className={ 'hambuger__icon--line' }></span>
+      <div id={"circle"}></div>
+      <SplashScreen />
+      <div className={"header"}>
+        <img
+          id={"logo"}
+          src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/dev-128.png"
+          alt="logo"
+        />
+        <div className={"hambuger"} onClick={() => setShow(!show)}>
+          <div className={"hambuger__icon"}>
+            <span className={"hambuger__icon--line"}></span>
+            <span className={"hambuger__icon--line"}></span>
           </div>
         </div>
       </div>
       <BrowserRouter>
-        <div className={ show ? 'nav nav--show' : 'nav nav--hide' }>
-          <div className={ 'nav__item' } onClick={ () => setShow(false) }>
-            <div className={ 'nav__item__wrap' }>
-              {/*<div>
-                <img
-                  src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/GitHub-Mark-Light-120px-plus.png"
-                  alt="github" id={ "logo--github" }
-                  onClick={ () => window.open('https://github.com/brave-people/Dev-Event-Client') }/>
-              </div>*/}
-              <Link to="/Dev-Event-Client" className={ 'nav__text' }>
+        <div className={show ? "nav nav--show" : "nav nav--hide"}>
+          <div className={"nav__item"} onClick={() => setShow(false)}>
+            <div className={"nav__item__wrap"}>
+              <Link to="/Dev-Event-Client" className={"nav__text"}>
                 EVENT
               </Link>
-              <Link to="/Dev-Event-Client/productPast" className={ 'nav__text' }>
+              <Link to="/Dev-Event-Client/productPast" className={"nav__text"}>
                 PAST EVENT
               </Link>
             </div>
           </div>
-          <div className={ 'nav__item--point' }></div>
+          <div className={"nav__item--point"}></div>
         </div>
         <Switch>
-          <Route exact path={ "/Dev-Event-Client" } component={ Home }/>
-          <Route path={ "/Dev-Event-Client/education" } component={ Education }/>
+          <Route exact path={"/Dev-Event-Client"} component={Home} />
+          <Route
+            exact
+            path={"/Dev-Event-Client/productPast"}
+            component={ProductPast}
+          />
         </Switch>
-        {/*<Navigation list={ [] }/>*/ }
       </BrowserRouter>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,98 +10,112 @@ type CardProps = {
 };
 
 function Card(props: CardProps) {
-  const { tags } = useSelector<ReducerType>(state => state.filter) as FilterModel;
-  const { activeTags } = useSelector<ReducerType>(state => state.filter) as FilterModel;
+  const { tags } = useSelector<ReducerType>(
+    (state) => state.filter
+  ) as FilterModel;
+  const { activeTags } = useSelector<ReducerType>(
+    (state) => state.filter
+  ) as FilterModel;
   const dispatch = useDispatch();
 
   const list: string[] = [];
-  props.cards.forEach(el => {
-    el.tags.split(',').forEach(tag => {
+  props.cards.forEach((el) => {
+    el.tags.split(",").forEach((tag) => {
       if (!list.includes(tag)) {
         list.push(tag);
       }
-    })
-  })
-  if (!tags.length) dispatch(addFilter(list))
+    });
+  });
+  if (!tags.length) dispatch(addFilter(list));
 
   function getDate(date: string) {
-    const sliceDate = date.split('.');
+    const sliceDate = date.split(".");
 
-    if (date.includes('T')) {
-      const sliceTime = sliceDate[sliceDate.length - 1].split('T');
+    if (date.includes("T")) {
+      const sliceTime = sliceDate[sliceDate.length - 1].split("T");
       const time = `${sliceTime[0]} ${sliceTime[1]}`;
       return `${sliceDate[1]}.${time}`;
     } else {
-      return date.split('.').splice(1, 2).join('.');
+      return date.split(".").splice(1, 2).join(".");
     }
   }
 
   function moveLink(link: string) {
-    window.open(link, '_blank');
+    window.open(link, "_blank");
   }
 
   function setTransCard(e: React.MouseEvent) {
     const target = e.target as HTMLTextAreaElement;
-    if (target.classList.contains('card')) {
-      // console.log('x: ', e.clientX, target.offsetLeft);
-      // console.log('y: ', e.clientY, target.offsetTop);
+    if (target.classList.contains("card")) {
       if (target.offsetLeft + target.clientWidth / 2 < e.clientX) {
-        target.style.transform = 'rotate3d(1, 1, 1, 1deg)';
+        target.style.transform = "rotate3d(1, 1, 1, 1deg)";
       } else {
-        target.style.transform = 'rotate3d(1, 1, 1, -1deg)';
+        target.style.transform = "rotate3d(1, 1, 1, -1deg)";
       }
     }
   }
 
   function setLeaveCard(e: React.MouseEvent) {
     const target = e.target as HTMLTextAreaElement;
-    if (target.classList.contains('card')) {
-      target.style.transform = 'rotate3d(1, 1, 1, 0)';
+    if (target.classList.contains("card")) {
+      target.style.transform = "rotate3d(1, 1, 1, 0)";
     }
   }
 
   return (
-    <div className={'card__column'}>
+    <div className={"card__column"}>
       {props.cards.map((el) => {
-
-        const splitTags = el.tags.split(',');
+        const splitTags = el.tags.split(",");
         const startDate = getDate(el.startDate);
         const endDate = getDate(el.endDate);
 
         // 필터 기능
         if (activeTags.length === 1) {
           // 하나만 선택했을 경우 하나라도 포함된 카드를 필터링 함
-          if (splitTags.findIndex(el => activeTags.includes(el)) === -1) {
+          if (splitTags.findIndex((el) => activeTags.includes(el)) === -1) {
             return false;
           }
         } else if (activeTags.length) {
           // 다중 선택했을 경우 모두 포함하는 카드를 필터링 함
-          if (!activeTags.every(v => splitTags.includes(v))) {
+          if (!activeTags.every((v) => splitTags.includes(v))) {
             return false;
           }
         }
 
         return (
-          <div key={el.id} className={'card'} style={{ backgroundImage: `url(${el.thumbnail})` }}
-               onClick={() => moveLink(el.link)} onMouseMove={setTransCard} onMouseLeave={setLeaveCard}>
-            <div className={'card__tag'}>
-              <img className={'card__tag__image'}
-                   src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/price-tag.svg" alt="tag"/>
-              <div style={{ display: 'block' }}>
-                {splitTags.map((tag, index) => <span key={index}>{tag}</span>)}
+          <div
+            key={el.id}
+            className={"card"}
+            style={{ backgroundImage: `url(${el.thumbnail})` }}
+            onClick={() => moveLink(el.link)}
+            onMouseMove={setTransCard}
+            onMouseLeave={setLeaveCard}
+          >
+            <div className={"card__tag"}>
+              <img
+                className={"card__tag__image"}
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.cindy.com/dev-event/price-tag.svg"
+                alt="tag"
+              />
+              <div style={{ display: "block" }}>
+                {splitTags.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
               </div>
             </div>
-            <div className={'card__text'}>
-              <p className={'card__text--date'}>{startDate} {el.endDate && <span> ~ {endDate}</span>}</p>
+            <div className={"card__text"}>
+              <p className={"card__text--date"}>
+                {startDate} {el.endDate && <span> ~ {endDate}</span>}
+              </p>
               <h2>{el.title}</h2>
               <p>{el.description}</p>
-              <p className={'font-medium'}>{el.owner}</p>
+              <p className={"font-medium"}>{el.owner}</p>
             </div>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default Card;
