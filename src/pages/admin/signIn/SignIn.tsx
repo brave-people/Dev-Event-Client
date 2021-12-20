67ef240df682387de5f71344cc79d8bd996428ae
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-12-20 23:44:55 +0900

    nextjs적용, admin front 분리

---
+++ src/pages/admin/signIn/SignIn.tsx
@@ -0,0 +1,11 @@
+const SignIn = () => {
+  return (
+    <div>
+      <h2>용감한 관리자</h2>
+      <label htmlFor="id">관리자 아이디</label>
+      <input id="id" type="text" />
+    </div>
+  );
+};
+
+export default SignIn;
