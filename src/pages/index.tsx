fe3a082129e1ddb5553a98053dd60fc4b023012e
leedoopal <1233wsh@gmail.com>
2022-01-11 22:37:15 +0900

    Home <Head> 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from 'next/head';
+
+// TODO:: favicon 추가
 const Home = () => {
-  return <div>front home</div>;
+  return (
+    <Head>
+      <title>Create Next App</title>
+      {/*<link rel="icon" href="/favicon.ico" />*/}
+    </Head>
+  );
 };
 
 export default Home;
