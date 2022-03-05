07a776d6dae731fbf64dc7a1706a679ad2d2877a
leedoopal <1233wsh@gmail.com>
2022-03-06 00:22:01 +0900

    #31 cors error FE 프록시 해결

--- next.config.js
+++ next.config.js
@@ -2,9 +2,25 @@ const path = require('path');
 const prod = process.env.NODE_ENV === 'production';
 const name = 'Dev-Event-Client';
 
-module.exports = {
+const nextConfig = {
+  webpack: (config) => {
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+    };
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: process.env.NEXT_PUBLIC_ADMIN_DOMAIN + '/:path*',
+      },
+    ];
+  },
   assetPrefix: prod ? `/${name}/` : '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
+
+module.exports = nextConfig;
