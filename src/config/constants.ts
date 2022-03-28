7a779c56dc749bd26665eeb53146484305ae5807
leedoopal <1233wsh@gmail.com>
2022-03-28 21:58:14 +0900

    #32 status_code 수정 반영

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
-export const STATUS_200 = 20001;
-export const STATUS_201 = 20101;
-export const STATUS_203 = 20003;
-export const STATUS_400 = 40001;
-export const STATUS_404 = 40004;
+export const STATUS_200 = 200;
+export const STATUS_201 = 201;
+export const STATUS_203 = 203;
+export const STATUS_400 = 401;
+export const STATUS_404 = 404;
 
 export const baseRouter = () =>
   process.env.NODE_ENV === 'production' ? '/Dev-Event-Client' : '';
