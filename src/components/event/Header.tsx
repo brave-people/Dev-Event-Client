2552ba7128580cc49d186c209c68da6e3ce7544a
leedoopal <1233wsh@gmail.com>
2022-04-07 21:44:26 +0900

    #36 회원 nav 추가, signOut 추가

--- src/components/event/Header.tsx
+++ src/components/event/Header.tsx
@@ -1,5 +1,9 @@
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import classNames from 'classnames';
 import type { UserInfoType } from '../../model/User';
 import getUserRoleIsAdmin from '../../util/get-user-role';
+import { baseRouter } from '../../config/constants';
 
 const Header = ({ user }: { user: UserInfoType }) => {
   const isAdmin = typeof user !== 'string' && getUserRoleIsAdmin(user?.roles);
@@ -7,12 +11,90 @@ const Header = ({ user }: { user: UserInfoType }) => {
   return (
     <header className="admin__header">
       <h1>용감한 관리자</h1>
-      <select name="user" id="user-options">
-        <option value="">회원 정보 수정</option>
-        <option value="">비밀번호 변경</option>
-        {isAdmin && <option value="">회원 관리</option>}
-        <option value="">로그아웃</option>
-      </select>
+      <Menu as="div" className="ml-3 relative">
+        <div>
+          <Menu.Button className="flex focus:outline-none focus:ring-2 focus:ring-offset-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              viewBox="0 0 20 20"
+              fill="#DEE1E5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'block px-4 py-2 text-sm text-gray-700'
+                  )}
+                >
+                  회원 정보 수정
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'block px-4 py-2 text-sm text-gray-700'
+                  )}
+                >
+                  비밀번호 변경
+                </a>
+              )}
+            </Menu.Item>
+            {isAdmin && (
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700'
+                    )}
+                  >
+                    회원 관리
+                  </a>
+                )}
+              </Menu.Item>
+            )}
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href={baseRouter() + '/auth/signOut'}
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'block px-4 py-2 text-sm text-gray-700'
+                  )}
+                >
+                  로그아웃
+                </a>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </header>
   );
 };
---
+++ src/pages/auth/signOut.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import type { NextPageContext } from 'next/types';
+import getToken from '../../server/api/auth/getToken';
+import { useDeleteCookie } from '../../util/use-cookie';
+import { baseRouter } from '../../config/constants';
+
+const signOut = () => {
+  const router = useRouter();
+  useEffect(() => {
+    useDeleteCookie(document, 'access_token');
+    useDeleteCookie(document, 'access_token_expired_at');
+    useDeleteCookie(document, 'refresh_token');
+    useDeleteCookie(document, 'refresh_token_expired_at');
+
+    router.push(baseRouter() + '/auth/signIn');
+  }, []);
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const cookies = context.req?.headers.cookie;
+  const token = await getToken(cookies);
+
+  // token이 없거나 에러나면 로그인 페이지로 이동
+  if (!token?.data || token?.error) {
+    return {
+      redirect: {
+        destination: '/auth/signIn',
+      },
+    };
+  }
+
+  return { props: { token: token.data } };
+};
+
+export default signOut;
