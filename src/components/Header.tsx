780a174b9c0f80d148302af11d74b9904ae81182
leedoopal <1233wsh@gmail.com>
2022-06-04 00:24:08 +0900

    #42 - header css 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ const Header = ({
       <h1>용감한 관리자</h1>
       <Menu as="div" className="ml-3 relative">
         <div>
-          <Menu.Button className="flex focus:outline-none focus:ring-2 focus:ring-offset-2">
+          <Menu.Button className="flex p-2 items-center bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-8 w-8"
@@ -37,6 +37,22 @@ const Header = ({
                 fillRule="evenodd"
                 d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
                 clipRule="evenodd"
+              />
+            </svg>
+            {user && (
+              <p className="ml-1 mr-3 font-bold text-gray-600">{user.name}</p>
+            )}
+            <svg
+              className="w-5 h-6"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="#687A92"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
               />
             </svg>
           </Menu.Button>
@@ -50,14 +66,14 @@ const Header = ({
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+          <Menu.Items className="origin-top-right absolute right-0 mt-2 p-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
             <Menu.Item>
               {({ active }) => (
                 <button
                   onClick={onClickModifyUser}
                   className={classNames(
-                    active ? 'bg-gray-100' : '',
-                    'block px-4 py-2 text-sm text-gray-700'
+                    active && 'bg-blue-50',
+                    'w-full block px-4 py-2 text-sm text-left text-gray-500 rounded font-bold'
                   )}
                 >
                   회원 정보 수정
@@ -69,8 +85,8 @@ const Header = ({
                 <a
                   href="/auth/password"
                   className={classNames(
-                    active ? 'bg-gray-100' : '',
-                    'block px-4 py-2 text-sm text-gray-700'
+                    active && 'bg-blue-50',
+                    'block px-4 py-2 text-sm text-gray-500 font-bold'
                   )}
                 >
                   비밀번호 변경
@@ -83,8 +99,8 @@ const Header = ({
                   <a
                     href="/auth/users"
                     className={classNames(
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700'
+                      active && 'bg-blue-50',
+                      'block px-4 py-2 text-sm text-gray-500 font-bold'
                     )}
                   >
                     회원 관리
@@ -97,8 +113,8 @@ const Header = ({
                 <a
                   href="/auth/signOut"
                   className={classNames(
-                    active ? 'bg-gray-100' : '',
-                    'block px-4 py-2 text-sm text-gray-700'
+                    active && 'bg-blue-50',
+                    'block px-4 py-2 text-sm text-gray-500 font-bold'
                   )}
                 >
                   로그아웃
