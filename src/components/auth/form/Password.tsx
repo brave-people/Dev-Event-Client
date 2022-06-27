6ab9035fde5a8cd46349e99a30503cfdf435d5e7
leedoopal <1233wsh@gmail.com>
2022-06-27 23:36:53 +0900

    #42 auth - password css 추가

--- src/components/auth/form/Password.tsx
+++ src/components/auth/form/Password.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { STATUS_200 } from '../../../config/constants';
 import { passwordApi } from '../../../pages/api/auth/users/password';
 import Input from '../../input/Input';
-import { STATUS_200 } from '../../../config/constants';
 
 const Password = () => {
   const router = useRouter();
@@ -23,7 +23,7 @@ const Password = () => {
     setCheckNewPassword(e.target.value);
   };
 
-  const save = async () => {
+  const submit = async () => {
     if (!password || !newPassword)
       return setErrorMessage(
         '현재 패스워드, 새로운 패스워드 모두 입력해주세요 :)'
@@ -41,33 +41,60 @@ const Password = () => {
     if (data.status_code === STATUS_200)
       return await router.push('/admin/event');
 
-    return alert(data.message);
+    return data.message && setErrorMessage(data.message);
   };
 
   return (
-    <div className="form--large">
-      <div className="form__content">
-        <Input
-          text="현재 비밀번호"
-          type="password"
-          value={password}
-          onChange={changePassword}
-        />
-        <Input
-          text="새 비밀번호"
-          type="password"
-          value={newPassword}
-          onChange={changeNewPassword}
-        />
-        <Input
-          text="새 비밀번호 확인"
-          type="password"
-          value={checkNewPassword}
-          onChange={changeCheckNewPassword}
-        />
-        <p>{errorMessage}</p>
+    <div className="list">
+      <div className="form--large">
+        <div className="form__content">
+          <Input
+            text="현재 비밀번호"
+            type="password"
+            value={password}
+            onChange={changePassword}
+          />
+          <Input
+            text="새 비밀번호"
+            type="password"
+            value={newPassword}
+            onChange={changeNewPassword}
+          />
+          <Input
+            text="새 비밀번호 확인"
+            type="password"
+            value={checkNewPassword}
+            onChange={changeCheckNewPassword}
+          />
+          {errorMessage && (
+            <div className="list__button--pop--right bg-red-500">
+              <button onClick={() => setErrorMessage('')}>
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+              {errorMessage}
+            </div>
+          )}
+        </div>
+        <button
+          className="form__button form__button--center w-20 inline-flex items-center justify-center my-4 p-2 rounded-md text-white bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          onClick={submit}
+        >
+          수정
+        </button>
       </div>
-      <button onClick={save}>수정</button>
     </div>
   );
 };
