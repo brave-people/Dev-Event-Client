b719d99b90646c35cb051ac8eb202696f2889e1d
leedoopal <1233wsh@gmail.com>
2022-09-06 23:36:36 +0900

    #49 replay - create 페이지 생성

---
+++ src/pages/admin/replay/create.tsx
@@ -0,0 +1,50 @@
+import 'react-datepicker/dist/react-datepicker.css';
+import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+import { stores } from '../../../store';
+import getToken from '../../../server/api/auth/getToken';
+import { getTagsApi } from '../../api/events/tag';
+import { useUpdateCookie } from '../../../util/use-cookie';
+import EventComponent from '../../../components/Event';
+import ReplayCreateForm from '../../../components/replay/Create';
+import type { NextPageContext } from 'next/types';
+import type { TokenModel } from '../../../model/User';
+
+const queryClient = new QueryClient();
+
+const EventCreate = ({ token }: { token: TokenModel }) => {
+  const setTags = useSetRecoilState(stores.tags);
+  const tagsData = async () => await getTagsApi();
+
+  useEffect(() => {
+    if (token?.access_token) useUpdateCookie(document, token);
+    tagsData().then((res) => setTags(res));
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <EventComponent title="개발행사 다시보기 등록">
+        <ReplayCreateForm />
+      </EventComponent>
+    </QueryClientProvider>
+  );
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const cookies = context.req?.headers.cookie;
+  const token = await getToken(cookies);
+
+  // token이 없거나 에러나면 로그인 페이지로 이동
+  if (!token?.data || token?.error) {
+    return {
+      redirect: {
+        destination: '/auth/signIn',
+      },
+    };
+  }
+
+  return { props: { token: token.data } };
+};
+
+export default EventCreate;
