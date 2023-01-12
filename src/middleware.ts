401e65c8d1cdf9b7fef29625035704839151f2ae
leedoopal <1233wsh@gmail.com>
2023-01-12 22:53:59 +0900

    middleware 생성

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
-import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
-  if (url.pathname == '/') {
-    return NextResponse.rewrite(new URL('/auth/signIn', request.url));
+  if (url.pathname === '/') {
+    return NextResponse.redirect(new URL('/auth/signIn', request.url));
   }
-  return NextResponse.rewrite(url);
+
+  return NextResponse.next();
 }
