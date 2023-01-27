9a0b3ba0bacefafce42615834c50ffb06969de64
leedoopal <1233wsh@gmail.com>
2023-01-27 23:40:17 +0900

    #134 template/Event 컴포넌트 분리

--- src/components/templates/Event.tsx
+++ src/components/templates/Event.tsx
@@ -6,6 +6,12 @@ import Header from '../layouts/Header';
 import Nav from '../layouts/Nav';
 import type { ReactElement } from 'react';
 
+const BackgroundBox = () => {
+  return (
+    <div className="z-10 bg-gray-300 block fixed top-0 left-0 opacity-60 w-full h-full" />
+  );
+};
+
 const Event = ({
   title,
   children,
@@ -23,12 +29,8 @@ const Event = ({
         <Nav />
         <section className="p-8 wrap__box--bg">
           <h1 className="mb-4 text-3xl font-bold">{title}</h1>
-          <>
-            {layer && (
-              <div className="z-10 bg-gray-300 block fixed top-0 left-0 opacity-60 w-full h-full" />
-            )}
-            {children}
-          </>
+          {layer && <BackgroundBox />}
+          {children}
         </section>
       </main>
     </Suspense>
