ad63618cb6daea19f0ea91c92e7eb11fd086375a
leedoopal <1233wsh@gmail.com>
2023-01-27 22:59:24 +0900

    #134 next/image 적용

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const path = require('path');
-const prod = process.env.NODE_ENV === 'production';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const nextConfig = {
+const prod = process.env.NODE_ENV === 'production';
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default {
   webpack: (config) => {
     return {
       ...config,
@@ -19,6 +22,7 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    domains: ['brave-people-3.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
-
-module.exports = nextConfig;
