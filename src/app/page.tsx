778d35b4f5e545033221b45e8335d8dbc07e271f
leedoopal <1233wsh@gmail.com>
2023-02-01 00:09:09 +0900

    #135 주석 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import getToken from '../server/api/auth/getToken';
-import type { NextPageContext } from 'next/types';
 
 const Home = () => {
   return (
@@ -9,26 +7,5 @@ const Home = () => {
     </Head>
   );
 };
-
-/*export const getServerSideProps = async (context: NextPageContext) => {
-  const cookie = context.req?.headers.cookie;
-  const token = await getToken(cookie);
-  console.log(token);
-
-  // token이 없거나 에러나면 로그인 페이지로 이동
-  if (!token?.data || token?.error) {
-    return {
-      redirect: {
-        destination: '/auth/signIn',
-      },
-    };
-  }
-
-  return {
-    redirect: {
-      destination: '/admin/event',
-    },
-  };
-};*/
 
 export default Home;
