6fc4233786543068a772b2f4c948f81f2d1b1098
resister-boy <hassanpumped17@gmail.com>
2024-06-10 20:39:50 +0900

    refactor: change dyanmic and add ref checker

--- src/components/molecules/editor/index.tsx
+++ src/components/molecules/editor/index.tsx
@@ -1,41 +1,36 @@
 import React, { Dispatch, forwardRef, SetStateAction, useMemo, useRef } from 'react';
 import 'react-quill/dist/quill.snow.css';
-import dynamic from 'next/dynamic';
 import { fetchUploadImage } from '../../../api/image';
+import RQ, { Quill } from "react-quill";
+import ImageCompress from 'quill-image-compress'
+import ImageResize from 'quill-image-resize-module-react';
 
-const ReactQuill = dynamic(
-  async () => {
-    const { default: RQ } = await import('react-quill');
-    const ImageCompress = await import('quill-image-compress');
-    const ImageResize = await import('quill-image-resize-module-ts');
+Quill.register('modules/imageCompress', ImageCompress);
+Quill.register('modules/resize', ImageResize);
 
-    RQ.Quill.register('modules/imageCompress', ImageCompress.default);
-    RQ.Quill.register('modules/imageResize', ImageResize.ImageResize);
+const EditorContainer = forwardRef<RQ, any>(function getEditor(props, ref) {
+  console.log("ref", ref)
+  
+  const newProps = {
+    ...props,
+    modules: {
+      ...props.modules,
+      // imageResize: {
+      //   parchment: RQ.Quill.import('parchment'),
+      //   modules: ['Resize', 'DisplaySize', 'Toolbar'],
+      // },
+      // imageCompress: {
+      //   quality: 0.9, // default
+      //   imageType: 'image/*', // default
+      //   debug: true, // default
+      //   suppressErrorLogging: false, // default
+      //   insertIntoEditor: true, // default
+      // },
+    },
+  };
+  return <RQ ref={ref} {...newProps} />;
+})
 
-    const editorContainer = forwardRef<typeof RQ, any>(function getEditor(props, ref) {
-      const newProps = {
-        ...props,
-        modules: {
-          ...props.modules,
-          imageResize: {
-            parchment: RQ.Quill.import('parchment'),
-            modules: ['Resize', 'DisplaySize', 'Toolbar'],
-          },
-          // imageCompress: {
-          //   quality: 0.9, // default
-          //   imageType: 'image/*', // default
-          //   debug: true, // default
-          //   suppressErrorLogging: false, // default
-          //   insertIntoEditor: true, // default
-          // },
-        },
-      };
-      return <RQ ref={ref} {...newProps} />;
-  })
-  return editorContainer
-},
-  { ssr: false, loading: () => <div>*에디터를 불러오는 중입니다...</div> }
-);
 
 const Editor = ({
   description,
@@ -44,7 +39,7 @@ const Editor = ({
   description: string;
   setDescription: Dispatch<SetStateAction<FormData | null>>;
 }) => {
-  const quillRef = useRef<typeof ReactQuill | null>(null);
+  const quillRef = useRef<RQ | null>(null);
 
   const uploadImage = async (blob: FormData) => {
     if (blob === null) return '';
@@ -58,6 +53,8 @@ const Editor = ({
     if (data.file_url) return data.file_url;
     return '';
   };
+
+  console.log("quillRef", quillRef)
 
   const imageHandler = () => {
     const input = document.createElement('input');
@@ -73,17 +70,23 @@ const Editor = ({
 
         console.log("file", file)
         const formData = new FormData();
-        
-        if (file) {
+
+        if (file && quillRef.current) {
           formData.append('images', file);
           const imageURL = await uploadImage(formData);
-          const range = quillRef.current.getEditorSelection();
-          quillRef.current
-            .getEditor()
-            .insertEmbed(range.index, 'image', imageURL);
+          const range = quillRef.current.getEditorSelection() ?? null;
 
-          quillRef.current.getEditor().setSelection(range.index + 1);
-          document.body.querySelector(':scope > input').remove();
+          if (range && range.index) {
+            quillRef.current
+              .getEditor()
+              .insertEmbed(range.index, 'image', imageURL);
+
+            quillRef.current.getEditor().setSelection(range.index + 1, range.length);
+            document.body.querySelector(":scope > input")?.remove()
+          } else {
+            console.log("quill range undeinfed")
+            throw new Error('quill range undeinfed')
+          }
         } else {
           console.log("file undefined")
           throw new Error('file undefined')
@@ -114,11 +117,11 @@ const Editor = ({
 
   return (
     <div className="form__content">
-      <ReactQuill
+      <EditorContainer
+        ref={quillRef}
         style={{ width: '100%', height: '500px', paddingTop: '10px' }}
         placeholder="내용을 입력해주세요. *드래그하면 이미지가 추가됩니다..."
         theme="snow"
-        forwardedRef={quillRef}
         value={description}
         onChange={setDescription}
         modules={modules}
