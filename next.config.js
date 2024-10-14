4d2f0c7e2e5ff0fad9f4a58c7deca902ace1a62c
resister-boy <hassanpumped17@gmail.com>
2024-10-14 20:52:53 +0900

    config: add dependencies & config

--- next.config.js
+++ next.config.js
@@ -16,6 +16,16 @@ export default removeImportsFun({
       mode: prod ? 'production' : 'development',
     };
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+
+    return config;
+  },
   async rewrites() {
     return [
       {
