9c9f1ad4eec5dd15692b36067bd001a2b3fcc9d1
고언약[KR Server Tech] <covenant.ko@kakao.com>
2025-01-14 21:09:24 +0900

    [feature/editor] 마크다운 에디터 추가, todo 목록 추가

--- src/components/molecules/form/ContentDescription.tsx
+++ src/components/molecules/form/ContentDescription.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useDropzone } from 'react-dropzone';
 import type { EventForm } from '../../../model/Event';
 import Input from '../../atoms/input/Input';
 import ErrorContext from '../../layouts/ErrorContext';
@@ -17,6 +20,14 @@ type ContentDescriptionProps = Pick<
   | 'changeEventLink'
 >;
 
+// todo 미리보기 버튼 클릭하면 텍스트 입력이 사라지게 변경
+// todo 행사 설명 라벨 추가
+// todo 위치 맨 아래로 내리기
+// todo 여백 추가
+// todo 사진 업로드 버튼 만들기
+// todo 실제 파일 업로드로 시도
+// todo 행사 등록시 파일 업로드하도록 변경
+
 const ContentDescription = ({
   title,
   changeTitle,
@@ -28,6 +39,27 @@ const ContentDescription = ({
   eventLink,
   changeEventLink,
 }: ContentDescriptionProps) => {
+  const [markdown, setMarkdown] = useState(description);
+  const [preview, setPreview] = useState(false);
+
+  const onDrop = (acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const text = reader.result as string;
+        setMarkdown((prev) => `${prev}\n![${file.name}](${text})`);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const handleMarkdownChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.target.value);
+    // changeDescription(e.target.value); // todo active me
+  };
+
   return (
     <>
       <Input
@@ -40,11 +72,30 @@ const ContentDescription = ({
         {error.title && <ErrorContext />}
       </Input>
       <div className="form__content">
-        <Input
-          text="행사 설명"
-          value={description}
-          onChange={changeDescription}
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>드래그 드랍 혹은 여기를 클릭하시면 사진을 추가할 수 있습니다.</p>
+        </div>
+        <textarea
+          className="w-full h-96 p-2 border border-gray-300 rounded-md resize-none"
+          value={markdown}
+          onChange={handleMarkdownChange}
+          placeholder="Enter your markdown here..."
         />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setPreview(!preview);
+          }}
+          className="bg-blue-500 text-white rounded py-2 px-5 text-sm font-medium mt-2"
+        >
+          {preview ? 'Edit' : 'Preview'}
+        </button>
+        {preview && (
+          <div className="markdown-preview mt-4 p-2 border border-gray-300 rounded-md">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </div>
+        )}
       </div>
       <div className="form__content">
         <Input
