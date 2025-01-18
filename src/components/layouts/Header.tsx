b9981b469c3de5343c50f9a1892b504854d1288c
고언약[KR Server Tech] <covenant.ko@kakao.com>
2025-01-18 11:47:54 +0900

    [feature/admin-ui-change] header 미고정 버그 수정

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -19,7 +19,7 @@ const Header = ({ user }: { user?: UsersModel | UserProfileModel }) => {
   };
 
   return (
-    <header className="header fixed top-0 left-0 right-0 bg-white border-b border-gray-100 z-50">
+    <header className="header top-0 left-0 right-0 bg-white border-b border-gray-100 z-50">
       <div className="max-w-11xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link
