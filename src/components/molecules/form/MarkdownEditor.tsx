2a4feebb79b6b1d0b4b03e62156b088762aa7182
고언약 <covenant.ko@kakao.com>
2025-07-14 08:22:05 +0900

    [feature/editor] [AI] 에디터 줄바꿈 이슈 수정
    이상해
    
    ---
    
    - W#32
    - asfdsaffsd
    sdf
    
    sdf
    
    sdf
    
    sdf
    
    이렇게 입력했어.
    미리보기 버튼을 클릭하니 첨부한 스크린샷처럼 이상하게 나와.
    수정해줘

--- src/components/molecules/form/MarkdownEditor.tsx
+++ src/components/molecules/form/MarkdownEditor.tsx
@@ -1,3 +1,4 @@
+import remarkBreaks from 'remark-breaks';
 import React, { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import ReactMarkdown from 'react-markdown';
@@ -7,10 +8,8 @@ interface MarkdownEditorProps {
   onMarkdownChange: (value: string) => void;
 }
 
-// todo 에디터에서 set 안됨
 // todo api 이미지 업로드 연동
 // todo 행사 등록시 파일 업로드하도록(lazy upload) 변경
-// todo 엔터 한번만 쳐도 줄바꿈
 // todo 이미지 편집 버튼과 구분하기 위해서 hr 버튼 추가
 
 const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
@@ -61,8 +60,81 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
         />
       )}
       {preview && (
-        <div className="markdown-preview h-96 mt-4 mb-4 p-2 border border-gray-300 rounded-md">
-          <ReactMarkdown>{markdown}</ReactMarkdown>
+        <div className="markdown-preview h-96 mt-4 mb-4 p-2 border border-gray-300 rounded-md overflow-auto">
+          <div
+            style={{
+              lineHeight: '1.6',
+            }}
+          >
+            <ReactMarkdown
+              remarkPlugins={[remarkBreaks]}
+              components={{
+                h1: ({ children }) => (
+                  <h1
+                    style={{
+                      fontSize: '2em',
+                      fontWeight: 'bold',
+                      marginBottom: '0.5em',
+                      marginTop: '0.5em',
+                    }}
+                  >
+                    {children}
+                  </h1>
+                ),
+                h2: ({ children }) => (
+                  <h2
+                    style={{
+                      fontSize: '1.5em',
+                      fontWeight: 'bold',
+                      marginBottom: '0.5em',
+                      marginTop: '0.5em',
+                    }}
+                  >
+                    {children}
+                  </h2>
+                ),
+                h3: ({ children }) => (
+                  <h3
+                    style={{
+                      fontSize: '1.25em',
+                      fontWeight: 'bold',
+                      marginBottom: '0.5em',
+                      marginTop: '0.5em',
+                    }}
+                  >
+                    {children}
+                  </h3>
+                ),
+                hr: () => (
+                  <hr
+                    style={{
+                      margin: '1em 0',
+                      border: 'none',
+                      borderTop: '1px solid #ccc',
+                    }}
+                  />
+                ),
+                ul: ({ children }) => (
+                  <ul style={{ paddingLeft: '1.5em', marginBottom: '1em' }}>
+                    {children}
+                  </ul>
+                ),
+                ol: ({ children }) => (
+                  <ol style={{ paddingLeft: '1.5em', marginBottom: '1em' }}>
+                    {children}
+                  </ol>
+                ),
+                li: ({ children }) => (
+                  <li style={{ marginBottom: '0.25em' }}>{children}</li>
+                ),
+                p: ({ children }) => (
+                  <p style={{ marginBottom: '1em' }}>{children}</p>
+                ),
+              }}
+            >
+              {markdown}
+            </ReactMarkdown>
+          </div>
         </div>
       )}
     </div>
