ab010a2c8c03a03bad70f5ad831197e3c4ca86d3
고언약 <covenant.ko@kakao.com>
2025-12-24 12:16:15 +0900

    [feature/editor-251223] 엔터 한번에 대해서 줄바꿈이 일어나도록 수정

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -7,9 +7,15 @@ import DOMPurify from 'dompurify';
  * blob: URL은 로컬 이미지 미리보기를 위해 허용합니다.
  */
 export const parseMarkdown = (markdown: string): string => {
+  // marked 설정: 단일 줄바꿈도 <br>로 변환 (GFM 스타일)
+  marked.setOptions({
+    breaks: true, // 한 번의 엔터로 줄바꿈 처리
+    gfm: true, // GitHub Flavored Markdown 활성화
+  });
+
   const rawHtml = marked.parse(markdown);
   return DOMPurify.sanitize(rawHtml as string, {
-    ADD_TAGS: ['img'],
+    ADD_TAGS: ['img', 'br'],
     ADD_ATTR: ['src', 'alt'],
     ALLOWED_URI_REGEXP:
       /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp|blob):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
