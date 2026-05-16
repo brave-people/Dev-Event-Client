be3acb767cf30a073e2a88ee5c30de3a640ac111
고언약 <covenant.ko@kakao.com>
2026-05-16 14:38:51 +0900

    `[release/1.5.1]` 빌드 실패 수정

--- src/server/api/auth/getToken.ts
+++ src/server/api/auth/getToken.ts
@@ -10,7 +10,7 @@ const decodeJwt = (token: string): JwtPayload => {
     const payload = parts[1].replace(/-/g, '+').replace(/_/g, '/');
     const padded = payload.padEnd(
       payload.length + ((4 - (payload.length % 4)) % 4),
-      '=',
+      '='
     );
     return JSON.parse(Buffer.from(padded, 'base64').toString('utf-8'));
   } catch {
